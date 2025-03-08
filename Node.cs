cff30cd09c1c843b8577e95e079e517e07c10e1c
helarye <helarye@gmail.com>
2025-03-08 22:22:33 +0200

    Add project files.

---
+++ Node.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace BagrutClass24q4
+{
+    public class Node<T>
+    {
+        private T value;//ערך החוליה
+        private Node<T> next;//מצביע על החוליה הבאה
+                             //פונקציה בונה שיוצרת חוליה עם ערך ללא חוליות אחריה
+        public Node(T value)
+        {
+            this.value = value;
+            next = null;
+        }
+        public Node(T value, Node<T> next)
+        {
+            this.value = value;
+            this.next = next;
+        }
+
+        public T GetValue()
+        {
+            return value;
+        }
+
+        public Node<T> GetNext()
+        {
+            return next;
+        }
+        //הפונק' מחזירה אמת אם לחוליה הנוכחית יש חוליה עוקבת
+        public bool HasNext()
+        {
+            return next != null;
+        }
+        //פונק' הקובעת את ערך החוליה הנוכחית
+        public void SetValue(T value)
+        {
+            this.value = value;
+        }
+        //פונק' הקובעת את החוליה העוקבת של החוליה הנוכחית
+        public void SetNext(Node<T> next)
+        {
+            this.next = next;
+        }
+        //פונקציה המכינה מחרוזת מכל החוליות המקושרות החל מן החוליה
+        //הנתונה
+        public override string ToString()
+        {
+            string str2return = "";
+            if (next == null)
+                str2return = value + "-->null";
+            else
+                str2return = value + "-->" + next;
+            return str2return;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using BagrutClass24q4;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        BusStation[] stArr = new BusStation[3];
+        stArr[0] = new BusStation(1, new int[] { 2, 5, 3 }, 3);
+        stArr[1] = new BusStation(2, new int[] { 1, 2, 3,4 }, 4);
+        stArr[2] = new BusStation(3, new int[] { 7, 2, 3, 4, 5 }, 5);
+
+        Node<int> busLst = AllStations(stArr);
+        Console.WriteLine(busLst);
+    }
+    
+
+    //סעיף ב
+    public static Node<int> AllStations(BusStation[] stArr)
+    {
+
+        Node<int> busLst = new Node<int>(0);//יוצרים חוליה תותבת שתוסר בסיום
+        Node<int> last = busLst;
+        int[] firstStnArr = stArr[0].GetArr();
+        int bussesAmount = stArr[0].GetAmount();
+        bool stoppingAll;
+
+        for (int i = 0; i < bussesAmount; i++)
+        {
+            stoppingAll=true;
+            for (int j = 1; j < stArr.Length; j++)
+            {
+                //Console.WriteLine(firstStnArr[i]);
+                if (!stArr[j].IsStopping(firstStnArr[i]))
+                    stoppingAll=false;
+            }
+            if (stoppingAll)
+            {
+                last.SetNext(new Node<int>(firstStnArr[i]));
+                last = last.GetNext();
+                Console.WriteLine(busLst);
+            }
+        }
+        busLst = busLst.GetNext();
+        return busLst;
+    }
+}
+
+public class BusStation
+
+{
+    private int num;
+    private int[] arr;
+    private int amount;
+        
+        public BusStation(int num, int[] arr, int amount)
+    {
+        this.num = num;
+        this.arr = arr;
+        this.amount = amount;
+    }
+    //סעיף א
+    public bool IsStopping(int n)
+    {
+        for (int i = 0; i < this.amount; i++)
+        {
+            if (this.arr[i] == n)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int[] GetArr()
+    {
+        return this.arr;
+    }
+
+    internal int GetAmount()
+    {
+        return this.amount;
+    }
+}
